903b6ceaff38dd83271f4700b61afb2352a7909a
ibw2h14akl <fabio.klement@bi.bib.de>
2016-01-17 21:06:52 +0100

    Überarbeitete Version

---
+++ GUI/GruppenController.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class GruppenController
+{
+	private Stage stage;
+	
+	@FXML
+	private Button goBackButton;
+	
+	@FXML 
+	private Button saveButton;
+	
+	@FXML 
+	private Button newButton;
+	
+	@FXML
+	void goBackClicked(ActionEvent e) throws IOException
+	{
+		FXMLLoader Loader = new FXMLLoader(getClass().getResource("hauptmenue.fxml"));
+		Parent rootMain = Loader.load();
+		Scene sceneMain = new Scene( rootMain,600,400);
+		stage.setTitle("Hauptmenü");
+		stage.setScene(sceneMain);
+		HauptmenueController controllerMain = 
+				Loader.<HauptmenueController>getController();
+		controllerMain.setStage(stage);
+	}
+	
+	@FXML
+	void saveClicked()
+	{
+		
+	}
+	
+	@FXML
+	void newClicked()
+	{
+		
+	}
+	
+	
+	public void setStage(Stage s) 
+	{
+		stage = s;
+	}
+}
