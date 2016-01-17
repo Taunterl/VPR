31a65eb612d507ff37fe5d3cb0c0eb4210c14fa0
ibw2h14akl <fabio.klement@bi.bib.de>
2016-01-17 21:00:39 +0100

    Rename und Layout geändert

--- GUI/RandomApp.java
+++ GUI/RandomApp.java
@@ -4,29 +4,27 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Login extends Application
+public class RandomApp extends Application
 {
 
 	public static void main(String[] args)
 	{
 		launch(args);
 	}
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception 
 	{
-
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
 		
 		primaryStage.setTitle("Login");
 		Parent root = fxmlLoader.load();
-		Scene loginScene = new Scene(root, 500, 300);
+		Scene loginScene = new Scene(root, 600, 200);
 		primaryStage.setScene(loginScene);
 		primaryStage.show();
 		
 		LoginController controller =
 				fxmlLoader.<LoginController>getController();
 		controller.setStage(primaryStage);
-	}
-		
-	
+	}	
 }
