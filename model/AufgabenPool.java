739e5e06d60e7d840382a49bee0ee40171e4ce66
ibw2h14ais <jannis.i@web.de>
2016-01-20 10:55:31 +0100

    Objektklasse von AufgabenPool

---
+++ model/AufgabenPool.java
@@ -0,0 +1,26 @@
+
+public class AufgabenPool {
+	
+	private int dozID;
+	private String fach;
+	private String beschreibung;
+	AufgabenPool(String beschreibung, int dozID, String fach){
+		
+		this.beschreibung = beschreibung;
+		this.dozID = dozID;
+		this.fach = fach;
+	}
+	
+	public String getBeschreibung(){
+		return this.beschreibung;
+	}
+	
+	public String getFach(){
+		return this.fach;
+	}
+	
+	public int getDozID(){
+		return this.dozID;
+	}
+	//Die AufgabenID benötigt noch eine methode zur Ermittlung der Aktuell vergebenen ID´s aus der Datenbank (id++)
+}
