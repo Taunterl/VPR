f84fa8c8c6e085b5dceb569016b78083783d61f6
ibw2h14ais <jannis.i@web.de>
2016-01-20 10:53:40 +0100

    Objektklasse von Aufgaben

---
+++ model/Aufgaben.java
@@ -0,0 +1,41 @@
+
+public class Aufgaben {
+	
+	private int id;
+	private String name;
+	private String description;
+	private String category;
+	private String pool;
+
+	
+	
+	Aufgaben(String name, String description, String category, String pool){
+		
+		this.name = name;
+		this.description = description;
+		this.category = category;
+		this.pool = pool;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public String getDescription(){
+		return this.description;
+	}
+	
+	public String getCategory(){
+		return this.category;
+	}
+	
+	public String getPool(){
+		return this.pool;
+	}
+	
+	
+	
+	
+	
+
+}
