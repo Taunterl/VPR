7e131f0b8f7fec363b65c5b4d3140df03976c03d
ibw2h14aha <tuan_an.ha@bi.bib.de>
2016-01-23 21:41:01 +0100

    Update AddTaskController.java

--- GUI/AddTaskController.java
+++ GUI/AddTaskController.java
@@ -26,6 +26,29 @@ public class AddTaskController
 	
 	@FXML
 	private Button addTask;
+	
+	@FXML
+	private Button addTaskButton;
+	
+	@FXML
+	private Button goBackButton;
+	
+	public void addTaskButtonClicked(ActionEvent e)
+	{
+		
+	}
+	
+	public void goBackClicked(ActionEvent e) throws IOException
+	{
+		FXMLLoader Loader = new FXMLLoader(getClass().getResource("aufgaben.fxml"));
+		Parent rootTasks = Loader.load();
+		Scene sceneTasks = new Scene(rootTasks,600,400);
+		stage.setTitle("Aufgaben");
+		stage.setScene(sceneTasks);
+		AufgabenController controllerTasks = 
+				Loader.<AufgabenController>getController();
+		controllerTasks.setStage(stage);
+	}
 
 	public void setStage(Stage s) {
 		this.stage = s;
